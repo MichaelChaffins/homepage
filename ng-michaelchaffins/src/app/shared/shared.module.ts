aeb1d5d93e96e802b44ac8e182ab25c9537fec62
Michael Chaffins <michael.chaffins@judicial.state.co.us.com>
2020-02-10 12:57:56 -0700

    projects page layout

--- ng-michaelchaffins/src/app/shared/shared.module.ts
+++ ng-michaelchaffins/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [],
@@ -15,7 +16,8 @@ import { MatCardModule } from '@angular/material/card';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatDividerModule
   ]
 })
 export class SharedModule { }
